e54d3fd1d490f356535c530f6cd3497b1bbeb076
cbxiii <chrisbradley@Chris-Bradley-2.local>
2013-10-21 17:22:15 +0100

    Initial commit

---
+++ parts/shared/html-footer.php
@@ -0,0 +1,8 @@
+
+	<!-- Linked Javascript files -->
+	<script src="<?php bloginfo('template_directory'); ?>/js/jquery-1.9.1.min.js"></script>
+	<script src="<?php bloginfo('template_directory'); ?>/js/site.js"></script>
+	
+	
+	</body>
+</html>
